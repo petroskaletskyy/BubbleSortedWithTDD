f936dde0d534b586e4c050641abbd2223a6be55e
petro-skaletskyy <capone.petro@gmail.com>
2020-01-21 22:46:31 +0200

    corrected remarks

--- src/com/example/BubbleSort.java
+++ src/com/example/BubbleSort.java
@@ -1,21 +1,21 @@
 package com.example;
 
 public class BubbleSort {
-
-	public int[] sort(int[] array) {
-		boolean swapped = true;
-		while (swapped) {
-			swapped = false;
-			for (int i = 0; i < array.length - 1; i++) {
-				if (array[i] > array[i + 1]) {
-					int temp = array[i];
-					array[i] = array[i + 1];
-					array[i + 1] = temp;
-					swapped = true;
-				}
-			}
-		}
-		return array;
-	}
+	
+  public int[] sort(int[] array) {
+    boolean swapped = true;
+    while (swapped) {
+      swapped = false;
+      for (int i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+          int temp = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = temp;
+          swapped = true;
+        }
+      }
+    }
+  return array;
+  }
 
 }
--- tests/com/example/BubbleSortTest.java
+++ tests/com/example/BubbleSortTest.java
@@ -9,37 +9,72 @@ public class BubbleSortTest {
 	BubbleSort sorter = new BubbleSort();
 
 	@Test
-	public final void shouldDoNothingWithEmptyArray() {
+	public void sortEmptyList_ShouldReturnEmptyList() {
 		int[] inputArray = {};
 		int[] expectedArray = {};
 		assertArrayEquals(expectedArray, sorter.sort(inputArray));
 	}
 
 	@Test
-	public final void shouldDoNothingWithOneElementArray() {
-		int[] inputArray = { 22 };
-		int[] expectedArray = { 22 };
+	public void sortListWithOneElement_ShouldReturnSortedList() {
+		int[] inputArray = {1};
+		int[] expectedArray = {1};
+		assertArrayEquals(expectedArray, sorter.sort(inputArray));
+	} 
+	
+	@Test
+	public void sortListWithTwoElementsInCorrectOrder_ShouldReturnSameList() {
+		int[] inputArray = {1, 2};
+		int[] expectedArray = {1, 2};
+		assertArrayEquals(expectedArray, sorter.sort(inputArray));
+	} 
+	
+	@Test
+	public void sortListWithTwoElementsInReverseOrder_ShouldReturnSortedList() {
+		int[] inputArray = {2, 1};
+		int[] expectedArray = {1, 2};
+		assertArrayEquals(expectedArray, sorter.sort(inputArray));
+	} 
+	
+	@Test
+	public void sortListWithSameTwoElements_ShouldReturnSortedList() {
+		int[] inputArray = {2, 2};
+		int[] expectedArray = {2, 2};
+		assertArrayEquals(expectedArray, sorter.sort(inputArray));
+	} 
+	
+	@Test
+	public void sortListWithThreeElementsInCorrectOrder_ShouldReturnSameList() {
+		int[] inputArray = {1, 2, 3};
+		int[] expectedArray = {1, 2, 3};
 		assertArrayEquals(expectedArray, sorter.sort(inputArray));
 	}
-
+	
 	@Test
-	public final void shouldSortArrayWithTwoElements() {
-		int[] inputArray = { 24, 12 };
-		int[] expectedArray = { 12, 24 };
+	public void sortListWithThreeElementsFirstTwoSwapped_ShouldReturnSortedList() {
+		int[] inputArray = {2, 1, 3};
+		int[] expectedArray = {1, 2, 3};
 		assertArrayEquals(expectedArray, sorter.sort(inputArray));
 	}
-
+	
 	@Test
-	public final void shouldSortArrayWithThreeElements() {
-		int[] inputArray = { 4, 0, 8 };
-		int[] expectedArray = { 0, 4, 8 };
+	public void sortListWithThreeElementsLastTwoSwapped_ShouldReturnSortedList() {
+		int[] inputArray = {1, 3, 2};
+		int[] expectedArray = {1, 2, 3};
 		assertArrayEquals(expectedArray, sorter.sort(inputArray));
 	}
-
+	
 	@Test
-	public final void shouldSortArrayWithFiveElements() {
-		int[] inputArray = { 9, 5, -5, 1, 3 };
-		int[] expectedArray = { -5, 1, 3, 5, 9 };
+	public void sortListWithThreeElementsReverseOrder_ShouldReturnSortedList() {
+		int[] inputArray = {3, 2, 1};
+		int[] expectedArray = {1, 2, 3};
+		assertArrayEquals(expectedArray, sorter.sort(inputArray));
+	}
+	
+	@Test
+	public void sortListWithNElementsRandomOrder_ShouldReturnSortedList() {
+		int[] inputArray = {2, 19, 0, -5, 2, 7, 1, -1, 3};
+		int[] expectedArray = {-5, -1, 0, 1, 2, 2, 3, 7, 19};
 		assertArrayEquals(expectedArray, sorter.sort(inputArray));
 	}
 }
